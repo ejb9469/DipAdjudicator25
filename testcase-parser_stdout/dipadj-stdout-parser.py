dd940b7c1fc887e5c912f778ae885eb9a13fb298
Evan B <ebouleris0002@gmail.com>
2026-09-06 09:02:41 -0400

    pruned old diagnostic code (Python)

--- testcase-parser_stdout/dipadj-stdout-parser.py
+++
@@ -1,5 +0,0 @@
-# This script will compare the stdout of a bunch of runs of DipAdj25,
-# ... using the random-ordered-orders TestCases option.
-# Then it will approximate the "success rate" of each TestCase
-
-# Anything lower than 100% is inadequate, unless for whatever reason this only happens with certain paradoxes that can be accounted for.
